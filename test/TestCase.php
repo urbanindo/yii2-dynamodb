4fdff2cf643e23aa31b1ddc219918abdae95dca9
Petra Barus <petra.barus@gmail.com>
2018-06-20 15:39:24 +0700

    Upgrade compatibility to 7.2.

--- test/TestCase.php
+++ test/TestCase.php
@@ -1,8 +1,9 @@
 <?php
 
 use yii\helpers\ArrayHelper;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends \PHPUnit_Framework_TestCase
+abstract class TestCase extends BaseTestCase
 {
 
     /**
